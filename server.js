e0e00ab87eff5116a3c5bcc06bf80bea1fb228e9
Malgorzata Kapuscinska <m_kapuscinska1@o2.pl>
2018-04-13 00:07:17 +0200

    Copy necessary files

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+
+var app = express();
+
+app.get('/', function(req, res) {
+	res.send('Hello world');
+});
+
+var server = app.listen(3000, function(){
+	console.log('Przykladowa aplikacja nasłuchuje na http://localhost: 3000');
+});
