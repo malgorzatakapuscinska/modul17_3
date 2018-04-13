e600d478c943d67d08fbe2279bfa99fd6471254a
Malgorzata Kapuscinska <m_kapuscinska1@o2.pl>
2018-04-13 11:32:26 +0200

    Add simple server

--- server.js
+++ server.js
@@ -3,9 +3,33 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res) {
-	res.send('Hello world');
+	console.log('Otrzymałem żądanie GET od strony głównej');
+	res.send('Identyfikator, który został dopisany to: ');
 });
 
-var server = app.listen(3000, function(){
-	console.log('Przykladowa aplikacja nasłuchuje na http://localhost: 3000');
+app.post('/', function(req, res) {
+	console.log('Otrzymałem rzadanie POST od strony głównej');
+	res.send('Hello POST!');
 });
+
+app.delete('/del_user', function(req, res){
+	console.log('Otrzymałem rządanie DELETE od strony /del_user');
+	res.send('Hello DELETE');
+});
+
+app.get('/list_user', function(req, res){
+	console.log('Otrzymałem rządanie GET od strony /list_user');
+	res.send('Strona z listą użytkowników');
+});
+
+app.get('/ab*cd', function(req, res){
+	conosle.log('Otrzymałem rządanie GET od strony /ab*cd');
+	res.send('Wzór pasuje');
+});
+
+app.listen(3000);
+
+app.use(function(req, res, next){
+	res.status(404).send('Wybacz, nie mogliśmy odnaleźć tego czego żądasz :-(')
+});
+
